6f8aabe1c1f5f1b53448b5ac6699306cc50768ae
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-11-17 21:10:55 +0200

    header change

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react";
+import {useHistory} from 'react-router-dom'
 
 import Routes from './routes'
-import Header from './components/Header'
 import Footer from './components/Footer'
+import logo from './assets/Logo/logoX.jpg'
 import "./styles/app.css";
 
 
 function App() {
+  const history = useHistory()
+  const goHome = () => {
+    history.push('/')
+  }
   return (
     <body>
-      <Header />
+      <div className="navDiv">
+      <img onClick={goHome} className="logo" src={logo} alt="logo" />
+      </div>
       <main>
         <Routes/>
       </main>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,14 +6,11 @@ import logo from "../../assets/logo.svg";
 import styles from "./Header.module.css";
 
 const Header = () => {
-  const history = useHistory()
-  const goHome = () => {
-    history.push('/')
-  }
+  
   
   return (
     <div className={styles.navbar}>
-      <img onClick={goHome} className={styles.logo} src={logo} />
+      {/* <img onClick={goHome} className={styles.logo} src={logo} /> */}
       {/* <Navbar /> */}
     </div>
   );
