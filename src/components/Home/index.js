e7d2b1cd188e987d42fc2c01b3c0d8ec4ca73bd2
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-11-19 19:39:13 +0200

    side-nav

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,23 @@
 import React from "react";
+import {Link} from 'react-scroll'
 
-import "./Home.module.css";
 import Header from "../Header";
 import Banner from "../Banner";
 import Footer from "../Footer";
 import About from "../About";
 import Projects from "../Projects";
 import Contact from '../Contact'
+import styles from "./Home.module.css";
 
 function Home() {
   return (
     <body>
       <main>
+        <div className={styles.nav}>
+          <div className={styles.navItems}><Link>About Me</Link> </div>
+          <div className={styles.navItems}><Link>Projects</Link></div>
+          <div className={styles.navItems}><Link>Contact</Link></div>
+        </div>
         <Banner />
         <About />
         <Projects />
