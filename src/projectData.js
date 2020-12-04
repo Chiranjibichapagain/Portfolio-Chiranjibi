34019082133d01903f4663d9842759a3ad5c9c75
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-12-04 22:47:37 +0200

    [feat] projects data file

---
+++ src/projectData.js
@@ -0,0 +1,47 @@
+export const projects = {
+    dev: [
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+    ],
+
+    design: [
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+        {
+            about: "some text",
+            images: ["image1", "image2", "image3", "image4"],
+            link:"url"
+        },
+    ]
+}
