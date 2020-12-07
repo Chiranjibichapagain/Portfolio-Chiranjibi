861a69f6e9904c10f50fa221ccbad60b29d0e842
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2020-12-07 22:58:42 +0200

    [feat] projects data

--- src/projectData.js
+++ src/projectData.js
@@ -7,14 +7,14 @@ const projects = {
             title:"E-commerce Website",
             about: "This is a MERN (MongoDB, Express, React, Node) stack project. This is a demo project i did to sharpen my skill in Full-Stack technologies. I did this project using Typescript. Technologies and libraries such as Redux, Google Authentication, Material UI, Context API are also used in this project. Features of this projects includes CRUD operations for user and product, google login, password reset, Admin roles (bann user, give user roles), add product to cart and checkout. The server side of the application is deployed in the Heroku and client side in the Netlify.",
             images: [image, image, image,image],
-            link:"https://www.linkedin.com/in/chiranjibi-chapagain-4aa57595/"
+            link:"https://e-commerce-proj.netlify.app/"
         },
         {
             id:"dev-2",
-            title:"Pjoject title",
+            title:"Contact-list App",
             about: "I learned most of the skills i have in Full Stack Web Development on my own. Online courses, youtube video, documentation were my source of information. I have earned my Bachelors degree in Business Management and Masters degree in Public Administration. Both from Finland. After i kinda failed to find a job related to my degrees, i started thinking of alternative career. I was always interested in the filed of IT and i thought of giving it a try. From the beginning of 2020 while Corona Virus was engaging the whole world, so the Web Development did to me.",
             images: [image, image, image,image],
-            link:"https://www.linkedin.com/in/chiranjibi-chapagain-4aa57595/"
+            link:"https://chiranjibi-contact-app.netlify.app/"
         },
         {
             id:"dev-3",
