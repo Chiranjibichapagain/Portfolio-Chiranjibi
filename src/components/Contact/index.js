dee245a8a4101b8072259b7a23f557b1843a5862
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-01-16 14:30:58 +0200

    [fix] another try

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,50 +1,61 @@
-import React from "react";
+import React, {useState} from "react";
 
+import Input from '../Input/'
+import Button from '../Button'
 import styles from "./Contact.module.css";
 import classes from "./Contact.module.css";
 
 
 const Contact = () => {
+  const [fields, setValues] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    message:''
+  })
 
-   return (
+  const handleChange = (e) => {
+    setValues({...fields, [e.target.name]:[e.target.value]})
+  }
+
+  const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+      }
+
+  const handleSubmit = (e) => {
+    fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...fields })
+          })
+            .then(() => alert("Success!"))
+            .catch(error => alert(error));
+
+          e.preventDefault();
+  }
+
+  return (
     <div className={styles.contactMain}>
       <div className={styles.formBody}>
       <h2 className={styles.heading}>Contact Form</h2>
         <h5 className={classes.intro}>Please Fill in the form to send me message or feedback.</h5>
-        <form name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
-        {/* <form name='contact' method='POST' data-netlify='true' className={styles.form}>
-          <Input name='first name' type="text" placeholder="E.g. Your Name" labelText={"First Name"} />
-          <Input name='last name' type="text" placeholder="E.g. Your Surname" labelText={"Last Name"} />
-          <Input name='email' type="email" placeholder="E.g. example@example.com" labelText={"Email"} />
+        <form onSubmit={handleSubmit} name='contact' method='POST' action='/contact-thanks' data-netlify-honeypot="bot-field" id='contact-form' data-netlify='true' className={styles.form}>
+          <input type="hidden" name="form-name" value="contact" />  
+          <Input value={fields.firstName} handleChange={handleChange} name='firstName' type="text" placeholder="E.g. Your Name" labelText={"First Name"} />
+          <Input value={fields.lastName} handleChange={handleChange}  name='lastName' type="text" placeholder="E.g. Your Surname" labelText={"Last Name"} />
+          <Input value={fields.email} handleChange={handleChange}  name='email' type="email" placeholder="E.g. example@example.com" labelText={"Email"} />
           <div className={styles.inputDiv}>
           <label className={styles.label} >Your Message</label>
-          <textarea name='message' placeholder="E.g. Your message to me" className={styles.input} cols="30" rows="7" />
+          <textarea value={fields.message} onChange={handleChange}  name='message' placeholder="E.g. Your message to me" className={styles.input} cols="30" rows="7" />
           </div>
 
           <div data-netlify-recaptcha='true'></div>
           <div className={styles.btnDiv} >
-          <Button type='submit' text="Send" btnStyle="primary" onClick={()=>handleClick} />
+          <Button type='submit' text="Send" btnStyle="primary"  />
           </div>
-        </form> */}
+        </form>
       </div>
     </div>
   );
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import styles from './Input.module.css'
 
-const Input = ({labelText, placeholder, type, name}) => {
+const Input = ({ handleChange, labelText, placeholder, type, name}) => {
     return (
         <div className={styles.inputDiv}>
             <label className={styles.label} >{labelText}</label>
-            <input name={name} type={type} placeholder={placeholder} className={styles.input} />
+            <input onChange={handleChange} name={name} type={type} placeholder={placeholder} className={styles.input} />
         </div>
     )
 }
