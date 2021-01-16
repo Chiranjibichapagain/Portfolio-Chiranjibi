9c85b929daf4df468d5028366b40f39d1567a6df
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-01-16 14:48:27 +0200

    [fix] try 3

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -25,6 +25,7 @@ const Contact = () => {
       }
 
   const handleSubmit = (e) => {
+    console.log('submitting!!!')
     fetch("/", {
             method: "POST",
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -41,7 +42,7 @@ const Contact = () => {
       <div className={styles.formBody}>
       <h2 className={styles.heading}>Contact Form</h2>
         <h5 className={classes.intro}>Please Fill in the form to send me message or feedback.</h5>
-        <form onSubmit={handleSubmit} name='contact' method='POST' action='/contact-thanks' data-netlify-honeypot="bot-field" id='contact-form' data-netlify='true' className={styles.form}>
+        <form onSubmit={handleSubmit} name='contact' method='post' action='/contact-thanks' data-netlify-honeypot="bot-field" id='contact-form' data-netlify='true' className={styles.form}>
           <input type="hidden" name="contact" value="contact" />  
           <Input value={fields.firstName} handleChange={handleChange} name='firstName' type="text" placeholder="E.g. Your Name" labelText={"First Name"} />
           <Input value={fields.lastName} handleChange={handleChange}  name='lastName' type="text" placeholder="E.g. Your Surname" labelText={"Last Name"} />
