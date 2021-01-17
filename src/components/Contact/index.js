b9bb6531848b498745dcd6092f8d48544862feaa
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-01-17 15:32:02 +0200

    [Fix] Netlify form submission added

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -7,36 +7,6 @@ import classes from "./Contact.module.css";
 
 
 const Contact = () => {
-  // const [fields, setValues] = useState({
-  //   firstName: '',
-  //   lastName: '',
-  //   email: '',
-  //   message:''
-  // })
-
-  // const handleChange = (e) => {
-  //   setValues({...fields, [e.target.name]:[e.target.value]})
-  // }
-
-  // const encode = (data) => {
-  //       return Object.keys(data)
-  //           .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
-  //           .join("&");
-  //     }
-
-  // const handleSubmit = (e) => {
-  //   console.log('submitting!!!')
-  //   fetch("/", {
-  //           method: "POST",
-  //           headers: { "Content-Type": "application/x-www-form-urlencoded" },
-  //           body: encode({ "form-name": "contact", ...fields })
-  //         })
-  //           .then(() => alert("Success!"))
-  //           .catch(error => alert(error));
-
-  //         e.preventDefault();
-  // }
-
   return (
     <div className={styles.contactMain}>
       <div className={styles.formBody}>
@@ -44,12 +14,12 @@ const Contact = () => {
         <h5 className={classes.intro}>Please Fill in the form to send me message or feedback.</h5>
         <form name='contact' method='post'  className={styles.form}>
           <input type="hidden" name="form-name" value="contact" />  
-          <Input   name='firstName' type="text" placeholder="E.g. Your Name" labelText={"First Name"} />
-          <Input    name='lastName' type="text" placeholder="E.g. Your Surname" labelText={"Last Name"} />
-          <Input    name='email' type="email" placeholder="E.g. example@example.com" labelText={"Email"} />
+          <Input name='firstName' type="text" placeholder="E.g. Your Name" labelText={"First Name"} />
+          <Input name='lastName' type="text" placeholder="E.g. Your Surname" labelText={"Last Name"} />
+          <Input name='email' type="email" placeholder="E.g. example@example.com" labelText={"Email"} />
           <div className={styles.inputDiv}>
           <label className={styles.label} >Your Message</label>
-          <textarea   name='message' placeholder="E.g. Your message to me" className={styles.input} cols="30" rows="7" />
+          <textarea name='message' placeholder="E.g. Your message to me" className={styles.input} cols="30" rows="7" />
           </div>
 
           <div data-netlify-recaptcha='true'></div>
