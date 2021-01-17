610a68b38f12f6ec59eb63035c2b0062abc87de0
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-01-17 15:19:28 +0200

    [fix] another way form 2

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 
 import Input from '../Input/'
 import Button from '../Button'
@@ -7,16 +7,16 @@ import classes from "./Contact.module.css";
 
 
 const Contact = () => {
-  const [fields, setValues] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    message:''
-  })
+  // const [fields, setValues] = useState({
+  //   firstName: '',
+  //   lastName: '',
+  //   email: '',
+  //   message:''
+  // })
 
-  const handleChange = (e) => {
-    setValues({...fields, [e.target.name]:[e.target.value]})
-  }
+  // const handleChange = (e) => {
+  //   setValues({...fields, [e.target.name]:[e.target.value]})
+  // }
 
   // const encode = (data) => {
   //       return Object.keys(data)
@@ -44,12 +44,12 @@ const Contact = () => {
         <h5 className={classes.intro}>Please Fill in the form to send me message or feedback.</h5>
         <form name='contact' method='post'  className={styles.form}>
           <input type="hidden" name="form-name" value="contact" />  
-          <Input value={fields.firstName}  name='firstName' type="text" placeholder="E.g. Your Name" labelText={"First Name"} />
-          <Input value={fields.lastName}   name='lastName' type="text" placeholder="E.g. Your Surname" labelText={"Last Name"} />
-          <Input value={fields.email}   name='email' type="email" placeholder="E.g. example@example.com" labelText={"Email"} />
+          <Input   name='firstName' type="text" placeholder="E.g. Your Name" labelText={"First Name"} />
+          <Input    name='lastName' type="text" placeholder="E.g. Your Surname" labelText={"Last Name"} />
+          <Input    name='email' type="email" placeholder="E.g. example@example.com" labelText={"Email"} />
           <div className={styles.inputDiv}>
           <label className={styles.label} >Your Message</label>
-          <textarea value={fields.message}  name='message' placeholder="E.g. Your message to me" className={styles.input} cols="30" rows="7" />
+          <textarea   name='message' placeholder="E.g. Your message to me" className={styles.input} cols="30" rows="7" />
           </div>
 
           <div data-netlify-recaptcha='true'></div>
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import styles from './Input.module.css'
 
-const Input = ({ handleChange, labelText, placeholder, type, name}) => {
+const Input = ({  labelText, placeholder, type, name}) => {
     return (
         <div className={styles.inputDiv}>
             <label className={styles.label} >{labelText}</label>
-            <input onChange={handleChange} name={name} type={type} placeholder={placeholder} className={styles.input} />
+            <input  name={name} type={type} placeholder={placeholder} className={styles.input} />
         </div>
     )
 }
