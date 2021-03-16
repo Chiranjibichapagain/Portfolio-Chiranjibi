eaf13b6dc285cbbf8c5c8421ae8621d696150ac4
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-03-16 19:26:37 +0200

    [Fix] page height-development page

--- src/projectData.js
+++ src/projectData.js
@@ -38,7 +38,7 @@ const projects = {
             about: "This is a quiz application. The quiz questions and answers are fetched from Open Trivia DB. Query parameters (numbers of questions, category, difficulty level and question type) are dynamically set with the user input and the quiz is generated autometically. User can anser the quiz by clicking the option: after an user selects an answe, answer will be checked and the right answers turns into green and wrong into red. next questions will be automatically loaded after 3s and at the end the final result data will be presented with pia chart. User should logged in to use this app.",
             images: [quiz1, quiz2, quiz3],
             link: "https://quizz-random.netlify.app/",
-            github:"https://github.com/Chiranjibichapagain/MERN-stack-e-commerce-APP"
+            github:"https://github.com/Chiranjibichapagain/Quiz-app"
         },
         {
             id:"dev-3",
