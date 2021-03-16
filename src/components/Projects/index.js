ec8aa9dcc8abc40cc2e63631c7f21afe5c67dbf4
Chiranjibichapagain <chiranjibichapagain@gmail.com>
2021-03-16 19:47:10 +0200

    [Feat] Description projects section

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -17,6 +17,13 @@ const Projects = () => {
     <>
       <div className={styles.projectMain}>
         <h2 className={styles.heading}>Projects</h2>
+        <p className={styles.texts}>Although i am a Full-Stack developer, i belive, i lean more towards
+        Front-End. Before i even started programming i have been doing graphic design as a hobby. Later during my
+        master's degree i came to know about designing tools such as Figma and Adobe XD. I started learning using
+        these tools on my own and in a short time i start designing User Interfaces using Figma. As a passionate
+        developer with interest in the Front-End side i feel that i should get my hands dirty designing user 
+        interfaces and i believe, it is vital to have OK design skills for a Front-End developer. Thus 
+        this sections includes both my Development and Design projects. </p>
         <div className={styles.projectTypes}>
           <div onClick={toUX} className={styles.project}>UX/UI Design</div>
           <div onClick={toDevelopment} className={styles.project}>Web Development</div>
